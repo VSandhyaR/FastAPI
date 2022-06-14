81fd24bf60a46c34e9b15d7bec1d4d37b2b1a1c0
VSandhyaR <sandhya.vollala@gmail.com>
2022-06-14 20:28:16 +0530

    postgresql

--- database.py
+++ database.py
@@ -1,20 +1,24 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234@127.0.0.1:3306/todoapp"  #mysql
-SQLALCHEMY_DATABASE_URL = "sqlite:///.todos.db"  #sqlite
+# SQLALCHEMY_DATABASE_URL = "sqlite:///.todos.db"  #sqlite
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:test1234@localhost/TodoApplicationDatabase"  #postgresql
 
-
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL
-# )  # creates db engine for other db's
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)  # creates db engine for sqlite
+    SQLALCHEMY_DATABASE_URL
+)  # creates db engine for other db's
+
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )  # creates db engine for sqlite
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)  # creates instance of db session
 Base = declarative_base()  # creates db model
